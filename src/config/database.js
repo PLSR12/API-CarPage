6ad0d31cfbf2249226f20605fb0fdcb96d3ca8a4
Pedro Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-04-27 10:47:45 -0300

    create config for databae and multer

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+	dialect: 'postgres',
+	host: 'localhost',
+	username: 'postgres',
+	password: 'postgres',
+	database: 'vehicles',
+	define: {
+		timespamps: true,
+		underscored: true,
+		underscoredAll: true,
+	},
+};
