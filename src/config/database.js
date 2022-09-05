f8975d290fe3dc4e7e90d2f4e4c08833b7edbf9b
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-09-05 10:26:38 -0300

    [*] updated password

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,13 @@
 module.exports = {
-	dialect: 'postgres',
-	host: 'containers-us-west-71.railway.app',
-	username: 'postgres',
-	password: 'idO6SFedwjQKCnjWEJsU',
-	database: 'railway',
-	port: 6701,
-	define: {
-		timespamps: true,
-		underscored: true,
-		underscoredAll: true,
-	},
-};
+  dialect: 'postgres',
+  host: 'containers-us-west-71.railway.app',
+  username: 'postgres',
+  password: 'f0VviEUn2Wb4tVyZE0gL',
+  database: 'railway',
+  port: 6701,
+  define: {
+    timespamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
