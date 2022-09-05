362e057733386079f55ea3edd4d52d433922e9bf
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-09-05 10:25:43 -0300

    [*] up

--- src/app.js
+++ src/app.js
@@ -1,31 +1,31 @@
-import express from 'express'
-import routes from './routes'
-import { resolve } from 'path'
 import cors from 'cors'
+import express from 'express'
+import { resolve } from 'path'
+import routes from './routes'
 
 import './database'
 
 class App {
-  constructor () {
+  constructor() {
     this.app = express()
     this.app.use(cors())
 
     this.middlewares()
     this.routes()
   }
-  middlewares () {
+  middlewares() {
     this.app.use(express.json())
     this.app.use(
       '/vehicle-file',
       express.static(resolve(__dirname, '..', 'uploads'))
     )
-  
+
     this.app.use(
       '/brands-file',
       express.static(resolve(__dirname, '..', 'uploads'))
     )
   }
-  routes () {
+  routes() {
     this.app.use(routes)
   }
 }
